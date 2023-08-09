c44428481f9e07dbbfce499ea7367be11ddaadbf
Ayenal hoque <ayenal8684@gmail.com>
2023-08-09 18:12:25 +0600

    add mysql driver and chage crud functionality

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-sql-driver/mysql"
 )
 
 type User struct {
@@ -15,13 +18,20 @@ type User struct {
 	Gender string `json:"gender"`
 }
 
-var user = []User{
-	{Id: 1, Name: "A", Email: "a@a.com", Gender: "male"},
-	{Id: 2, Name: "B", Email: "b@b.com", Gender: "female"},
-	{Id: 3, Name: "C", Email: "c@c.com", Gender: "male"},
-}
+var db *sql.DB
 
 func getAllUser(c *gin.Context) {
+	var user []User
+	rows, err := db.Query("SELECT * FROM users")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var u User
+		rows.Scan(&u.Id, &u.Name, &u.Email, &u.Gender)
+		user = append(user, u)
+	}
 	c.IndentedJSON(http.StatusOK, user)
 }
 func postUser(c *gin.Context) {
@@ -29,39 +39,73 @@ func postUser(c *gin.Context) {
 	if err := c.BindJSON(&newUser); err != nil {
 		return
 	}
-	user = append(user, newUser)
+	result, err := db.Exec("INSERT INTO users (name, email, gender) VALUES (?, ?, ?)", newUser.Name, newUser.Email, newUser.Gender)
+	if err != nil {
+		log.Fatal(err)
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		log.Fatal(err)
+	}
+	newUser.Id = int(id)
 	c.IndentedJSON(http.StatusCreated, newUser)
 }
 func findUserById(c *gin.Context) {
+	var user User
 	userId, _ := strconv.Atoi(c.Param("id"))
-	for _, u := range user {
-		if u.Id == userId {
-			c.IndentedJSON(http.StatusOK, u)
-			return
-		}
+	row := db.QueryRow("SELECT * FROM users WHERE id = ?", userId)
+	err := row.Scan(&user.Id, &user.Name, &user.Email, &user.Gender)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "user not found"})
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "user not found"})
+	c.IndentedJSON(http.StatusOK, user)
 
 }
 
-func removeUserByIndex(c *gin.Context) {
-	index, _ := strconv.Atoi(c.Param("index"))
-	fmt.Println("user length ", len(user))
-	if len(user) >= index {
-		user = append(user[:index], user[index+1:]...)
-		c.IndentedJSON(http.StatusOK, gin.H{"message": "user deleted"})
+func removeUserById(c *gin.Context) {
+	userId, _ := strconv.Atoi(c.Param("id"))
+	result, err := db.Exec("DELETE FROM users WHERE id = ?", userId)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "user not found"})
 		return
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "user not found"})
+	_, err = result.RowsAffected()
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "user not found"})
+		return
+	}
+	c.IndentedJSON(http.StatusOK, gin.H{"message": "user deleted"})
 
 }
 
 func main() {
+
+	cfg := mysql.Config{
+		User:                 "root",
+		Passwd:               "",
+		Net:                  "tcp",
+		Addr:                 "127.0.0.1:3306",
+		DBName:               "golang_api",
+		AllowNativePasswords: true,
+	}
+
+	// Get a database handle.
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Connected!")
 	router := gin.Default()
 	router.GET("/users", getAllUser)
 	router.POST("/users", postUser)
 	router.GET("/users/:id", findUserById)
-	router.DELETE("/users/:index", removeUserByIndex)
+	router.DELETE("/users/:id", removeUserById)
 	router.Run("localhost:8080")
 	fmt.Println("All users:", user)
 }
